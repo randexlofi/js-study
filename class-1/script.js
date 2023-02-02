6b7408dd620b18a5269823741a359aedba6fb3a9
randexlofi <eupedrojose@gmail.com>
2023-02-02 13:20:14 +0000

    new file:   class-1/README.md modified:   class-1/script.js

--- class-1/script.js
+++ class-1/script.js
@@ -1,3 +1,11 @@
-window.alert('Test')
-window.confirm('Are you liking?')
-window.prompt('What is your name?')
+// To get the name displayed on screen.
+/*
+var name = window.prompt('What is your name?')
+window.alert('Welcome, ' + name + '!')
+*/
+
+//To sum 2 numbers
+var num1 = Number.parseInt(window.prompt('Number 1: '))
+var num2 = Number.parseInt(window.prompt('Number 2: '))
+var sum = num1+num2
+window.alert(num1 + ' + ' + num2 + ' = ' + sum)
