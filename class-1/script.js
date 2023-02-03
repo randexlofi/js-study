11a6ee5931c7319f0f4fefba74a9d6288ece3321
randexlofi <eupedrojose@gmail.com>
2023-02-03 14:09:13 +0000

    deleted:    class-1/README.md modified:   class-1/script.js

--- class-1/script.js
+++ class-1/script.js
@@ -4,8 +4,18 @@ var name = window.prompt('What is your name?')
 window.alert('Welcome, ' + name + '!')
 */
 
+//======================================================//
+
 //To sum 2 numbers
-var num1 = Number.parseInt(window.prompt('Number 1: '))
-var num2 = Number.parseInt(window.prompt('Number 2: '))
+/*
+To force set to int | Number.parseInt()
+To force set to float | Number.parseFloat()
+To let js decide | Number()
+*/
+
+var num1 = Number(window.prompt('Number 1: '))
+var num2 = Number(window.prompt('Number 2: '))
 var sum = num1+num2
-window.alert(num1 + ' + ' + num2 + ' = ' + sum)
+window.alert(`${num1} + ${num2} = ${sum}`)
+
+//======================================================//
