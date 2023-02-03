7811857925f5b85929a9e05e700f41417afbd3b8
randexlofi <eupedrojose@gmail.com>
2023-02-03 14:25:24 +0000

    modified:   class-2/index.html new file:   class-2/script.js new file:   class-2/style.css

---
+++ class-2/script.js
@@ -0,0 +1,4 @@
+var name = window.prompt('What\'s your name')
+document.write(`Your name has <u>${name.length}</u> letters. <br>`)
+document.write(`Your name with the function <b>toUpperCase()</b> is <u>${name.toUpperCase()}</u><br>`)
+document.write(`Your name with the function <b>toLowerCase()</b> is <u>${name.toLowerCase()}</u><br>`)
